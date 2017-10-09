be1101d29196f8fa09772f0d3be5396cefb9e41e
Luthfi <vlootfie@gmail.com>
2017-10-09 10:50:37 +0700

    anchor 2 completed

---
+++ week-2/anchor-menggunakan-if-else.js
@@ -0,0 +1,18 @@
+var nama, peran;
+
+nama = 'C2';
+
+console.log("Selamat datang di dunia Proxytia, " + nama);
+
+if (nama == 'C1'){
+    peran = 'Ksatria';
+    console.log("Halo Ksatria, " + nama + ', kamu dapat menyerang dengan senjatamu!');
+} else if(nama == 'C2'){
+    peran = 'Tabib';
+    console.log("Halo Tabib " + nama + ", kamu akan membantu temanmu yang terluka.");
+} else if(nama == 'C3'){
+    peran = 'Penyihir';
+    console.log("Halo Penyihir " + nama + ", ciptakan keajaiban yang membantu kemenanganmu!");
+} else if (typeof nama == 'undefined' || nama == null){
+    console.log("Nama tidak boleh kosong\nPilih peranmu untuk memulai game");    
+};
