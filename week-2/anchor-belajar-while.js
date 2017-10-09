1b3586297749494f54c75b1daa8c22973af27a80
Luthfi <vlootfie@gmail.com>
2017-10-09 12:14:04 +0700

    anchor-belajar-while.js completed

---
+++ week-2/anchor-belajar-while.js
@@ -0,0 +1,11 @@
+var num = 2;
+while(num <= 20) {
+  console.log(num + " - I love coding");
+  num+=2;
+}
+
+num = 20;
+while(num >= 2){
+    console.log(num + ' - I will become fullstack developer');
+    num-=2;
+}
