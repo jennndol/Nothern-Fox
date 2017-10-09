4571fe8ef741443bc7aef3f72d5544f0111161df
Luthfi <vlootfie@gmail.com>
2017-10-09 12:23:47 +0700

    anchor-belajar-for completed

---
+++ week-2/anchor-belajar-for.js
@@ -0,0 +1,7 @@
+for (var num = 1;num <= 20;num++) {
+  console.log(num + " - I love coding");
+};
+
+for (var num = 20;num >=1 ;num--) {
+    console.log(num + " - I will become fullstack developer");
+};
