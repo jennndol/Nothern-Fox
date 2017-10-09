fbdaddb2c9509bc7af77231ff0abf289e858d944
Luthfi <vlootfie@gmail.com>
2017-10-09 13:36:01 +0700

    ganjil-genap-dll hopefully completed

--- week-2/anchor-belajar-for.js
+++ week-2/anchor-belajar-for.js
@@ -2,6 +2,6 @@ for (var num = 1;num <= 20;num++) {
   console.log(num + " - I love coding");
 };
 
-for (var num = 20;num >=1 ;num--) {
+for (num = 20;num >=1 ;num--) {
     console.log(num + " - I will become fullstack developer");
 };
---
+++ week-2/ganjil-genap-dll.js
@@ -0,0 +1,38 @@
+var tebakan;
+for (var num = 1;num <=100 ;num++) {
+    tebakan = num%2;
+    if (tebakan==1){
+        console.log("GANJIL");
+    }
+    else{
+        console.log("GENAP");
+    }
+};
+
+for (num = 1;num <=100 ;num+=2) {
+    if (num%3==0){
+        console.log(num + " KELIPATAN 3");    
+    }
+    else{
+        console.log(" ");
+    }
+};
+
+
+for (num = 1;num <=100 ;num+=5) {
+    if (num%6==0){
+        console.log(num + " KELIPATAN 6");    
+    }
+    else{
+        console.log(" ");
+    }
+};
+
+for (num = 1;num <=100 ;num+=9) {
+    if (num%10==0){
+        console.log(num + " KELIPATAN 10");    
+    }
+    else{
+        console.log(" ");
+    }
+};
