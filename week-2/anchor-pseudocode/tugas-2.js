edce8f59d364e8bbddca880adb08e163a1e3a682
Luthfi <vlootfie@gmail.com>
2017-10-10 09:29:18 +0700

    tugas-2.js completed

---
+++ week-2/anchor-pseudocode/tugas-2.js
@@ -0,0 +1,10 @@
+// SIMPAN 'alas' dari segitiga
+// SIMPAN 'tinggi' dari segitiga
+// HITUNG 'luas segitiga' dari 'alas' dikali 'tinggi' dibagi 2
+// TULISKAN "Luas Segitiga: 'luas segitiga'"
+
+var alas = 3;
+var tinggi = 4;
+
+luas = (alas * tinggi) / 2;
+console.log("Luas segitiga: " + luas); 
