d17651d0ccb44215c83402b97a6b94311a4dded1
Luthfi <vlootfie@gmail.com>
2017-10-10 09:26:39 +0700

    tugas-1.js completed

---
+++ week-2/anchor-pseudocode/tugas-1.js
@@ -0,0 +1,13 @@
+// SIMPAN 'nama'
+// SIMPAN 'angka pembilang'
+// SIMPAN 'angka penyebut'
+// HITUNG 'hasil bagi' dari 'angka pembilang' dibagi 'angka penyebut'
+// TULISKAN "Halo 'nama', 'angka pembilang' dibagi 'angka penyebut' adalah sama dengan 'hasil bagi'
+
+var nama = "Luthfi";
+var angkaPembilang = 12;
+var angkaPenyebut = 3;
+
+hasilBagi = angkaPembilang / angkaPenyebut;
+
+console.log("Halo "+ nama + ", " + angkaPembilang + " dibagi " + angkaPenyebut + " adalah sama dengan " + hasilBagi);
