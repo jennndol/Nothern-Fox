4e710c34fce66371176f7f6e50a0887f41da432c
Luthfi <vlootfie@gmail.com>
2017-10-15 22:48:58 +0700

    add gitignore

--- week-2/js-application.js
+++ week-2/js-application.js
@@ -1,7 +1,4 @@
-function getRandom(){
-    return Math.floor(Math.random() * 3) + 1;
-}
-
+// call readline module
 var readline = require('readline');
 
 var rl = readline.createInterface({
@@ -9,33 +6,97 @@ var rl = readline.createInterface({
     output: process.stdout
 });
 
-var numbers = [];
+// declare variables
+var randomNumbers = [];
 var guessNumbers = [];
 var rightNumbers = [];
 
-rl.question('Please enter your name : ', function(name){
-    console.log('Hey '+ name + '...!\nWelcome to guess number quiz\nPlease enter your guess numbers (between 1 until 3)');
-    rl.question('Your first number here! : ', function(number1){
-        guessNumbers.push(number1);        
-        numbers.push(getRandom());
-        rl.question('Your second number here! : ', function(number2){
-            guessNumbers.push(number2);
-            numbers.push(getRandom());
-            rl.question('Your third number here! ', function(number3){
-                guessNumbers.push(number3);
-                numbers.push(getRandom());
-                console.log('Random Numbers : ' + numbers);
-                console.log('Guess Numbers :' + guessNumbers);
-                
-                for(var i=0; i<3; i++){
-                    if(guessNumbers[i] == numbers[i]){
+// get name and show it
+rl.question('\nPlease enter your name : ', function(args){
+    console.log('Hey '+ args + '...!\nWelcome to guess the numbers quiz\nPlease enter your guess numbers (between 1 until 3)');
+    console.log('\n');
+    
+    // get first number, generate random number and save it into array
+    rl.question('Your first number here! : ', function(args){
+        guessNumbers.push(args);        
+        randomNumbers.push(getRandom());
+
+        // get second number, generate random number and save it into array
+        rl.question('Your second number here! : ', function(args){
+            guessNumbers.push(args);
+            randomNumbers.push(getRandom());
+
+            // get third number, generate random number and save it into array
+            rl.question('Your third number here! ', function(args){
+                guessNumbers.push(args);
+                randomNumbers.push(getRandom());
+
+                // Match the guessNumbers and randomNumbers and save into rightNumbers if there is right number
+                for(var i=0; i<guessNumbers.length; i++){
+                    if(guessNumbers[i] == randomNumbers[i]){
                         rightNumbers.push(guessNumbers[i]);
                     }
                 }
-                console.log(rightNumbers);
-                console.log('correct answers persentage : '+ Math.floor(Number(rightNumbers.length)/Number(guessNumbers.length)*100) + '%');
+
+                // Show the result
+                console.log('\nHere is the result');
+                console.log('-------------------------------');
+                console.log('Guess numbers :' + guessNumbers);
+                console.log('Random numbers : ' + randomNumbers);
+                console.log('-------------------------------');                
+                console.log('So, you have ' + getTotalRightNumbers(rightNumbers));
+                console.log('correct number persentage : '+ getPercentage(rightNumbers.length, guessNumbers.length));
+                console.log('\n');
+
+                // close the readline interface
                 rl.close();            
             });
         });
     });
 });
+
+
+/*
+function getRandom: used to get random number in range between 1 until 3
+*/
+function getRandom(){
+    return Math.floor(Math.random() * 3) + 1;
+}
+
+/*
+function getPercentage: used to get percentage of something
+*/
+function getPercentage(x, y){
+    return Math.floor(Number(x)/Number(y)*100) + '%';
+}
+
+/*
+function getTotalRightNumbers: used to get the result of total in string of the correct numbers
+*/
+function getTotalRightNumbers(x){ // where x is the rightnumbers
+    var correctAnswerTotal = x.length;
+    var message;
+
+    // Check if total of correct answers is more than 0
+    if (correctAnswerTotal>0){
+        message = correctAnswerTotal + ' correct '+ numberOrNumbers(correctAnswerTotal) +',\nAnd those numbers are ' + x;
+    }
+    else {
+        message = 'no correct numbers';
+    }
+    return message;
+}
+
+/*
+function numberOrNumbers: used to get word 'number' or 'numbers' depends on the input
+*/
+function numberOrNumbers(x){ // where x is the number
+    var message;
+    if (x > 1){
+        message = 'numbers';
+    }
+    else{
+        message = 'number';
+    }
+    return message;
+}
