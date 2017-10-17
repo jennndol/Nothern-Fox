249cb93f53f9fa99ae2e941e36f512ab515e47f9
Luthfi <vlootfie@gmail.com>
2017-10-17 12:25:13 +0700

    challenge-konversi-menit.js completed

--- week-3/challenge-bandingkan-angka.js
+++ week-3/challenge-bandingkan-angka.js
@@ -3,14 +3,13 @@ function bandingkanAngka(angka1, angka2){
         return true;
     }  else if (angka2 < angka1) {
         return false;
-    } else {
+    } else if (angka2 === angka1) {
         return -1;
     }
 }
   
-// TEST CASES
-console.log(bandingkanAngka(5, 8)); // true
-console.log(bandingkanAngka(5, 3)); // false
-console.log(bandingkanAngka(4, 4)); // -1
-console.log(bandingkanAngka(3, 3)); // -1
-console.log(bandingkanAngka(17, 2)); // false
+console.log(bandingkanAngka(5, 8));
+console.log(bandingkanAngka(5, 3));
+console.log(bandingkanAngka(4, 4));
+console.log(bandingkanAngka(3, 3));
+console.log(bandingkanAngka(17, 2));
---
+++ week-3/challenge-x-dan-o.js
@@ -0,0 +1,10 @@
+function xo(str) {
+    // you can only write your code here!
+}
+  
+// TEST CASES
+console.log(xo('xoxoxo')); // true
+console.log(xo('oxooxo')); // false
+console.log(xo('oxo')); // false
+console.log(xo('xxxooo')); // true
+console.log(xo('xoxooxxo')); // true
