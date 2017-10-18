be9b0d7df79ab858014ba6b6cc91bf1ca35459b9
Luthfi <vlootfie@gmail.com>
2017-10-19 00:15:31 +0700

    fix declaring variable

--- week-2/anchor-pseudocode-2/tugas-3.js
+++ week-2/anchor-pseudocode-2/tugas-3.js
@@ -9,7 +9,7 @@
 
 var bintang = ' ';
 
-for (index1 = 1; index1<=10; index1++){
+for (var index1 = 1; index1<=10; index1++){
     for (index2 = 0; index2<=index1; index2++){
         bintang = '*';
     }
