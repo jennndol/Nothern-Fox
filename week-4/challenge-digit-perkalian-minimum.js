a845e59ca27ae2ef75dbf3625543bba608ea4166
Luthfi <vlootfie@gmail.com>
2017-10-24 13:20:58 +0700

    challenge-digit-perkalian-minimum.js completed

---
+++ week-4/challenge-digit-perkalian-minimum.js
@@ -0,0 +1,19 @@
+function digitPerkalianMinimum(angka) {
+    var tempNumbers = [];
+    for(var i=1; i<=angka; i++){
+        if(angka % i === 0){
+            var j = angka / i;
+            var lengthOfChars = (String(i) + String(j)).length;
+            tempNumbers.push(lengthOfChars);
+        }
+    }
+    tempNumbers.sort()
+    return tempNumbers[0];
+}
+
+// TEST CASES
+console.log(digitPerkalianMinimum(24)); // 2
+console.log(digitPerkalianMinimum(90)); // 3
+console.log(digitPerkalianMinimum(20)); // 2
+console.log(digitPerkalianMinimum(179)); // 4
+console.log(digitPerkalianMinimum(1)); // 2
