add1585e4103462cac3d2aee77649515a4263289
Luthfi <vlootfie@gmail.com>
2017-10-24 10:01:26 +0700

    challenge-cari-median.js completed

--- week-4/challenge-cari-median.js
+++ week-4/challenge-cari-median.js
@@ -1,10 +1,16 @@
 function cariMedian(arr) {
-    // you can only write your code here!
-  }
-  
-  // TEST CASES
-  console.log(cariMedian([1, 2, 3, 4, 5])); // 3
-  console.log(cariMedian([1, 3, 4, 10, 12, 13])); // 7
-  console.log(cariMedian([3, 4, 7, 6, 10])); // 7
-  console.log(cariMedian([1, 3, 3])); // 3
-  console.log(cariMedian([7, 7, 8, 8])); // 7.5
+    var median = 0;
+    if(arr.length % 2 !== 0){
+        median = arr[Math.round(arr.length/2)-1];
+    } else {
+        median = (arr[Math.floor(arr.length/2)-1] + arr[Math.round(arr.length/2)])/2;
+    }
+    return median;
+}
+
+// TEST CASES
+console.log(cariMedian([1, 2, 3, 4, 5])); // 3
+console.log(cariMedian([1, 3, 4, 10, 12, 13])); // 7
+console.log(cariMedian([3, 4, 7, 6, 10])); // 7
+console.log(cariMedian([1, 3, 3])); // 3
+console.log(cariMedian([7, 7, 8, 8])); // 7.5
