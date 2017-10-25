3bab2bbb4919c5e7a00e18a9f15856f207af6eb4
Luthfi <vlootfie@gmail.com>
2017-10-25 22:39:04 +0700

    challenge-ubah-huruf.js completed

---
+++ week-4/challenge-ubah-huruf.js
@@ -0,0 +1,14 @@
+function ubahHuruf(kata) {
+    var result = '';
+    for(var i=0; i<kata.length; i++){
+        result += String.fromCharCode(kata[i].charCodeAt(0) + 1);
+    }    
+    return result;
+}
+
+// TEST CASES
+console.log(ubahHuruf('wow')); // xpx
+console.log(ubahHuruf('developer')); // efwfmpqfs
+console.log(ubahHuruf('javascript')); // kbwbtdsjqu
+console.log(ubahHuruf('keren')); // lfsfo
+console.log(ubahHuruf('semangat')); // tfnbohbu
