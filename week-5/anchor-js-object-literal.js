a1213d8b0e684ce8ceab6382d17dca7132848a0b
Luthfi <vlootfie@gmail.com>
2017-11-05 01:17:50 +0700

    add anchor-js-object-literal.js

---
+++ week-5/anchor-js-object-literal.js
@@ -0,0 +1,15 @@
+var student = {
+    name: 'Alex',
+    age: 13,
+    dateOfBirth: '18-08-2000',
+    gender:'Male',
+    studentID: 101010,
+    hobbies: ['Swimming', 'Singing'],
+}
+
+console.log(student.name);
+console.log(student.age);
+console.log(student.dateOfBirth);
+console.log(student.gender);
+console.log(student.studentID);
+console.log(student.hobbies);
