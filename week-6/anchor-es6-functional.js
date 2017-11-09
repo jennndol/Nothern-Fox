ef5678153e76d290497d23efb9e0fb49f19462c6
Luthfi <vlootfie@gmail.com>
2017-11-09 12:49:58 +0700

    tugas 2 completed

--- week-6/anchor-es6-functional.js
+++ week-6/anchor-es6-functional.js
@@ -21,8 +21,9 @@ console.log(transformArrToLowerCase(['BUDI', 'MArNi', 'mukIDi'])); // ['budi', '
 var filterArrUnder10 = arr => {
     // Filter setiap element array yang nilainya dibawah 10, sisakan yang 10 ke atas!
     let result = arr.filter((isiArray) => {
-        return arr>10;
+        return isiArray > 10;
     })
+    return result;
 };
 
 console.log(filterArrUnder10([1, 5, 12, 15, 20])); // [12, 15, 20]
