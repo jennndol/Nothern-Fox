160e00fa3450d0f50c7fbc02af6b0e92943dc0c5
Luthfi <vlootfie@gmail.com>
2017-11-09 12:48:55 +0700

    tugas 1 completed

--- week-6/anchor-es6-functional.js
+++ week-6/anchor-es6-functional.js
@@ -5,7 +5,7 @@
 
 var transformArrToLowerCase = arr => {
     // Ubah setiap element array menjadi lowercase! 
-    let newArr = arr.map((isiArray)=> {
+    let newArr = arr.map((isiArray) => {
         return isiArray.toLowerCase();
     })
     return newArr;
@@ -20,6 +20,9 @@ console.log(transformArrToLowerCase(['BUDI', 'MArNi', 'mukIDi'])); // ['budi', '
 
 var filterArrUnder10 = arr => {
     // Filter setiap element array yang nilainya dibawah 10, sisakan yang 10 ke atas!
+    let result = arr.filter((isiArray) => {
+        return arr>10;
+    })
 };
 
 console.log(filterArrUnder10([1, 5, 12, 15, 20])); // [12, 15, 20]
