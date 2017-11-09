12f334878578151c517f63459068e5c7f6d8c367
Luthfi <vlootfie@gmail.com>
2017-11-09 12:47:21 +0700

    tugas 1 completed

--- week-6/anchor-es6-functional.js
+++ week-6/anchor-es6-functional.js
@@ -4,7 +4,11 @@
  */
 
 var transformArrToLowerCase = arr => {
-    // Ubah setiap element array menjadi lowercase!
+    // Ubah setiap element array menjadi lowercase! 
+    let newArr = arr.map((isiArray)=> {
+        return isiArray.toLowerCase();
+    })
+    return newArr;
 };
 
 console.log(transformArrToLowerCase(['BUDI', 'MArNi', 'mukIDi'])); // ['budi', 'marni', 'mukidi']
