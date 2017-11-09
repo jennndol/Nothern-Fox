904f341859141e305ec243c8f3f082559de77e53
Luthfi <vlootfie@gmail.com>
2017-11-09 12:30:05 +0700

    tugas 1 completed

--- week-6/anchor-es5-to-es6.js
+++ week-6/anchor-es5-to-es6.js
@@ -1,36 +1,36 @@
 /**
  * Tugas 1
  */
-var phi = 3.14;
-var power = 2;
-var radius = 0;
-var calculateArea = function (radius) {
+const phi = 3.14;
+const power = 2;
+let radius = 0;
+const calculateArea = function (radius) {
     return phi * Math.pow(radius, power);
 }
 
 radius = 21;
-var area21 = calculateArea(radius);
+let area21 = calculateArea(radius);
 
 radius = 7;
-var area7 = calculateArea(radius);
+let area7 = calculateArea(radius);
 
 console.log('area with 21 radius: ' + area21 + ', and area with 7 radius: ' + area7);
 
-/**
- * Tugas 2
- */
+// /**
+//  * Tugas 2
+//  */
 
-const multiply = function (num1, num2) {
-    return num1 * num2;
-}
+// const multiply = function (num1, num2) {
+//     return num1 * num2;
+// }
 
-const divide = function (num1, num2) {
-    return num1 / num2;
-}
+// const divide = function (num1, num2) {
+//     return num1 / num2;
+// }
 
-const doubleMe = function (num) {
-    return num * 2;
-}
+// const doubleMe = function (num) {
+//     return num * 2;
+// }
 
-console.log(multiply(5, 2));
-console.log(divide(10, 2));
+// console.log(multiply(5, 2));
+// console.log(divide(10, 2));
