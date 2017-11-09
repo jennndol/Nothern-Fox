a9fb9e50b964a805dd84db3943ff6fd266441108
Luthfi <vlootfie@gmail.com>
2017-11-09 12:39:07 +0700

    add anchor-es6-functional.js

---
+++ week-6/anchor-es6-functional.js
@@ -0,0 +1,31 @@
+/**
+ * Tugas 1
+ * Buatlah sebuah code yang mentransformasi array menggunakan map!
+ */
+
+var transformArrToLowerCase = arr => {
+    // Ubah setiap element array menjadi lowercase!
+};
+
+console.log(transformArrToLowerCase(['BUDI', 'MArNi', 'mukIDi'])); // ['budi', 'marni', 'mukidi']
+
+/**
+ * Tugas 2
+ * Buatlah sebuah code yang melakukan filter ke array menggunakan filter!
+ */
+
+var filterArrUnder10 = arr => {
+    // Filter setiap element array yang nilainya dibawah 10, sisakan yang 10 ke atas!
+};
+
+console.log(filterArrUnder10([1, 5, 12, 15, 20])); // [12, 15, 20]
+
+/**
+ * Tugas 3
+ * Buatlah sebuah code yang mendapatkan nilai terbesar menggunakan reduce!
+ */
+
+var reduceFindMaximum = arr => {
+    // kembalikan sebuah nilai yang mengandung angka terbesar dengan reduce!
+}
+console.log(reduceFindMaximum([5, 1, 7, 6, 9])); // 9
