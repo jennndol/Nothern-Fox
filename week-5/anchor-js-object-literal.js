0cc2b8bbc7c01be7c2acee0f57f3520443f48813
Luthfi <vlootfie@gmail.com>
2017-11-09 11:34:29 +0700

    fix the code by adding functions

--- week-5/anchor-js-object-literal.js
+++ week-5/anchor-js-object-literal.js
@@ -1,15 +1,37 @@
-var student = {
-    name: 'Alex',
-    age: 17,
-    dateOfBirth: '18-08-2000',
-    gender:'Male',
-    studentID: 101010,
-    hobbies: ['Swimming', 'Singing'],
+var studentObj = {
+    name: 'Alexandra',
+    age: 20,
+    dateofBirth: '14 Januari 1997',
+    gender: 'FEMALE',
+    studentID: '101010',
+    hobbies: ['Soccer'],
+    setName: function (teks) {
+        studentObj.name = teks;
+    },
+    setAge: function (angka) {
+        studentObj.age = angka;
+    },
+    setDateOfBirth: function (teks) {
+        studentObj.dateofBirth = teks;
+    },
+    setGender: function (teks) {
+        if ((teks === 'MALE') || (teks === 'FEMALE')) {
+            studentObj.gender = teks;
+        }
+    },
+    removeHobby: function (teks) {
+        studentObj.hobbies.splice(studentObj.hobbies.indexOf(teks));
+    },
+    getData: function () {
+        return studentObj;
+    }
 }
 
-console.log(student.name);
-console.log(student.age);
-console.log(student.dateOfBirth);
-console.log(student.gender);
-console.log(student.studentID);
-console.log(student.hobbies);
+console.log(studentObj);
+
+studentObj.setName('Luthfi');
+studentObj.setAge(25);
+studentObj.setDateOfBirth('8 Februari 1992');
+studentObj.setGender('MALE');
+studentObj.removeHobby('Soccer');
+console.log(studentObj.getData());
